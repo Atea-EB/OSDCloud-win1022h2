f158ded8fb72932b6a426f45a385ad4cec0b1d55
Atea-EB <elliot.berggren@atea.se>
2024-09-23 09:48:46 +0200

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -42,3 +42,8 @@ Write-Host "Starting OSDCloud" -ForegroundColor Green
 write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
 
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot

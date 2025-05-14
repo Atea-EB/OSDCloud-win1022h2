46ef9f117472fdbd35bc0e34821b2e3aed9414d2
Atea-EB <elliot.berggren@atea.se>
2025-05-14 15:03:53 +0200

    trying Logistics h hash

--- config.ps1
+++ config.ps1
@@ -56,6 +56,230 @@ write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud with Windows 11 24h2 sv-se"
 Start-OSDCloud -OSVersion 'Windows 11' -OSLanguage sv-se -OSBuild 24H2 -OSEdition Enterprise -ZTI
 
+#Hardware Hash
+
+
+function Get-HardwareHash
+ {
+    $hardwareHash = ""  
+    $deviceDetail = (Get-WMIObject -ComputerName @($env:ComputerName) -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'")
+ 
+    if($deviceDetail) {
+        $hardwareHash = $deviceDetail.DeviceHardwareData
+    }
+    else {
+        throw "Failed to retrive hardware hash."
+    }
+
+    return $hardwareHash
+}
+
+function Get-SerialNumber 
+{
+    $serialNumber = ""
+    $deviceBios = (Get-WmiObject -ComputerName @($env:ComputerName) -Class Win32_BIOS)
+
+    if($deviceBios) {
+        $serialNumber = $deviceBios.SerialNumber
+    }
+    else {
+        throw "Failed to retrive serial number."
+    }
+
+    return $serialNumber
+}
+
+function Get-ManufacturerName 
+{
+    $manufacturerName = "";
+    $deviceComputerSystem = (Get-WmiObject -ComputerName @($env:ComputerName) -Class Win32_ComputerSystem)
+
+    if($deviceComputerSystem) {
+        $manufacturerName = $deviceComputerSystem.Manufacturer
+    }
+    else {
+        throw "Failed to retrive manufacturer"
+    }
+
+    return $manufacturerName
+}
+
+function Get-AEPManufacturerIdForWap
+{
+    $manufacturerId = 0; 
+    $manufacturers = @(
+        [PSCustomObject]@{Name = "Acer"; Id = 27},
+        [PSCustomObject]@{Name = "Dell"; Id = 28},
+        [PSCustomObject]@{Name = "HP"; Id = 29},
+        [PSCustomObject]@{Name = "Lenovo"; Id = 30},
+        [PSCustomObject]@{Name = "Microsoft Corporation"; Id = 31},
+        [PSCustomObject]@{Name = "Asus"; Id = 32},
+        [PSCustomObject]@{Name = "Dynabook"; Id = 33},
+        [PSCustomObject]@{Name = "Fujitsu"; Id = 34},
+        [PSCustomObject]@{Name = "Getac"; Id = 35},
+        [PSCustomObject]@{Name = "Panasonic"; Id = 36},
+        [PSCustomObject]@{Name = "Samsung"; Id = 51}
+    )
+
+    try {
+        $localManufacturerName = Get-ManufacturerName    
+    }
+    catch {
+        throw $PSItem
+    }
+
+    foreach($manufacturer in $manufacturers) 
+    {
+        if($localManufacturerName -eq $manufacturer.Name) {
+            $manufacturerId = $manufacturer.Id
+        }
+    }
+
+    if($manufacturerId -eq 0) {
+        throw "Failed to find manufacturer id"
+    }
+
+    return $manufacturerId
+}
+
+function Get-JTConnectionString
+{
+    # Read credentials from CSV. 
+    $credentials = Import-Csv $PSScriptRoot"\JT-Credentials.csv"
+
+    Write-Host $credentials.server
+
+    $connectionString = "server=$($credentials.server);database=$($credentials.database);user id=$($credentials.user);password=$($credentials.password)"
+
+    return $connectionString
+}
+
+function Save-HardwareHashInJT
+{
+    $serialNumber = Get-SerialNumber
+    $hardwareHash = Get-HardwareHash
+
+    # Prepare connection. 
+    $connectionString = Get-JTConnectionString
+    $sqlConnection = New-Object System.Data.SQLClient.SQLConnection($connectionString)
+
+    # Define call to procedure. 
+    $sqlCommand = New-Object System.Data.SqlClient.SqlCommand   
+    $sqlCommand.Connection = $sqlConnection 
+    $sqlCommand.CommandType = [System.Data.CommandType]::StoredProcedure
+    $sqlCommand.CommandText = "JT2000.dbo.JT_AddSerialHash"
+    $sqlCommand.CommandTimeout = 120
+
+    # Define input/output parameters. 
+    $sqlCommand.Parameters.Add("@serial", [System.Data.SqlDbType]::varchar) | out-Null
+    $sqlCommand.Parameters['@serial'].Direction = [System.Data.ParameterDirection]::Input
+    $sqlCommand.Parameters['@serial'].value = $serialNumber
+    $sqlCommand.Parameters.Add("@hash", [System.Data.SqlDbType]::varchar) | out-Null
+    $sqlCommand.Parameters['@hash'].Direction = [System.Data.ParameterDirection]::Input
+    $sqlCommand.Parameters['@hash'].value = $hardwareHash
+    $sqlCommand.Parameters.Add("@returncode", [System.Data.SqlDbType]::int) | out-Null
+    $sqlCommand.Parameters['@returncode'].Direction = [System.Data.ParameterDirection]::Output
+    $sqlCommand.Parameters['@returncode'].value = -1
+
+    # Open connection and inject into db. 
+    Try {
+        $sqlConnection.Open()
+        $sqlCommand.ExecuteNonQuery() | out-null
+        $sqlConnection.Close()
+    }
+    Catch {
+        throw $PSItem
+    }
+}
+
+function Save-ToIntuneCsv ($append = $false)
+{
+    $serialNumber = Get-SerialNumber
+    $hardwareHash = Get-HardwareHash
+
+    $deviceProperties = [ordered] @{
+        "Device Serial Number" = $serialNumber
+        "Windows Product ID" = ""
+        "Hardware Hash" = $hardwareHash
+        "Manufacturer name" = ""
+        "Device model" = ""
+    }
+
+    $device = New-Object psObject -Property $deviceProperties
+
+    try {
+        if($append) {
+            $devices = @()
+    
+            $devices += $device
+            $devices += Import-Csv $PSScriptRoot"\devices.csv"
+            $devices | ConvertTo-Csv -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Out-File -FilePath $PSScriptRoot"\devices.csv"
+        }
+        else {
+            $device | ConvertTo-Csv -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Out-File -FilePath $PSScriptRoot"\devices.csv"
+        }
+    }
+    catch {
+        throw "Failed to save to devices.csv"
+    }
+}
+
+function Save-ToAEPCsv ($append = $false)
+{
+    $device = [PSCustomObject]@{
+        Serial = ""
+        UnusedOne = ""
+        Manufacturer = ""
+        ModelName = ""
+        DeviceId = ""
+        UnusedTwo = ""
+        HardwareHash = ""
+    }
+
+    try {
+        $device.Serial = Get-SerialNumber
+        $device.HardwareHash = Get-HardwareHash
+        $device.Manufacturer = Get-AEPManufacturerIdForWap
+    }
+    catch {
+        throw $PSItem
+    }
+
+    try {
+        if($append) {
+            $devices = @()
+    
+            $devices += $device
+            $devices += Import-Csv $PSScriptRoot"\devices.csv"
+            $devices | ConvertTo-Csv -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Out-File -FilePath $PSScriptRoot"\devices.csv"
+        }
+        else {
+            $device | ConvertTo-Csv -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Out-File -FilePath $PSScriptRoot"\devices.csv"
+        }
+    }
+    catch {
+        throw "Failed to save to devices.csv"
+    }
+}
+
+try {
+    $OutputPath = "D:\devices.csv"
+    Save-ToIntuneCsv -Path $OutputPath .\MSAP
+    Write-Host "Saved: $(Get-SerialNumber) to $OutputPath"
+}
+catch {
+    Write-Host $PSItem.ToString()
+}
+
+
+Write-Host "Press any key to continue.."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+
+
+
+
+
 
 
 #Restart from WinPE
